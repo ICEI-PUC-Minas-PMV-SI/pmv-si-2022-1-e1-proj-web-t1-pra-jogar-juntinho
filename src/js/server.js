a322eb70fd6b33b462b1429ccf3a5237781d253a
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-05-27 21:41:14 -0300

    mais configurações adicionais ao servidor

--- src/js/server.js
+++ src/js/server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running')
+  console.log('JSON Server esta rodando! Acesso em: http://localhost:3000')
 })
 
 // cmd => node server.js
