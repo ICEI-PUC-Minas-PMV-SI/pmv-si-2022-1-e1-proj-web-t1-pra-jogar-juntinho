6ce1ec84d1f90b7aae8d38069270c48cfca56652
Filipe Augusto <filipe.silva@sga.pucminas.br>
2022-05-29 16:36:32 -0300

    em andamento login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,53 +1,77 @@
+// import usuariosLista from "./../db/usuarios.json";
+
 // URL DA API DE DADOS
 URL = "http://localhost:3002/usuarios"; //Usuários cadastrados
 
-const pegarUsu = document.getElementById("pegar-usu");
+const logar = document.getElementById("logar");
+var localStorage = Window.localStorage;
+let nome = document.getElementById("nome").value;
+let senha = document.getElementById("senha").value;
+let validacao = false;
+let usuario = {};
 
-pegarUsu.addEventListener(
-  "click",
-  (evt) => {
-    fetch(URL, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: null,
-        nome: document.getElementById("nome").value,
-        senha: document.getElementById("senha").value,
-      }),
+logar.addEventListener("click", (evt) => {
+  // console.log("teste", usuariosLista.json());
+  fetch(URL)
+    .then((res) => {
+      console.log(res);
+      res.json();
     })
-      .then((res) => res.json())
-      .then(() => location.reload());
-  },
-  false
-);
+    .then((usuarios) => {
+      console.log(usuarios);
+      validacao = usuarios.some((u) => {
+        if (u.nome === nome && u.senha === senha) {
+          usuario = u;
+          return true;
+        } else {
+          return false;
+        }
+      });
 
-function logar() {
-  let usuario = document.getElementById("nome");
-  let senha = document.getElementById("senha");
-  let listaUser = [];
-  let userValid = {
-    nome: " ",
-    senha: " ",
-  };
+      if (validacao) {
+        localStorage.setItem(
+          "usuario",
+          JSON.stringify({
+            id: usuario.id,
+            nome: usuario.nome,
+            senha: usuario.senha,
+          })
+        );
+      } else {
+        alert("Usuário e/ou senha não encontrado, tente novamente");
+        return false;
+      }
 
-  listaUser = JSON.parse(URL);
+      window.location.href = "http://127.0.0.1:5500/src/index.html";
+    })
+    .catch((error) => console.error(error));
+});
 
-  listaUser.ForEach((item) => {
-    if (usuario.value == item.nome && senha.value == item.senha) {
-      userValid = {
-        nome: item.nome,
-        senha: item.senha,
-      };
-    }
-  });
+// function logar() {
+//   let usuario = document.getElementById("nome");
+//   let senha = document.getElementById("senha");
+//   let listaUser = [];
+//   let userValid = {
+//     nome: " ",
+//     senha: " ",
+//   };
 
-  if (usuario.value != userValid.usuario && senha.value != userValid.senha) {
-    usuario.setAttribute("style", "color-red");
-    senha.setAttribute("style", "color-red");
-    alert("Usuário não cadastrado");
-  } else {
-    window.location.href = "/src/index.html";
-  }
-}
+//   listaUser = JSON.parse(URL);
+
+//   listaUser.ForEach((item) => {
+//     if (usuario.value == item.nome && senha.value == item.senha) {
+//       userValid = {
+//         nome: item.nome,
+//         senha: item.senha,
+//       };
+//     }
+//   });
+
+//   if (usuario.value != userValid.usuario && senha.value != userValid.senha) {
+//     usuario.setAttribute("style", "color-red");
+//     senha.setAttribute("style", "color-red");
+//     alert("Usuário não cadastrado");
+//   } else {
+//     window.location.href = "/src/index.html";
+//   }
+// }
