bfc22ee5fb742811d7ff54da93212d2ec2c43c2a
Vitória Teixeira Alves de Souza <vitoria.souza.1397398@sga.pucminas.br>
2022-05-29 20:08:54 -0300

    Ajustes entrega 03

---
+++ src/js/edicao-perfil.js
@@ -0,0 +1,46 @@
+let usuario = JSON.parse(window.localStorage.getItem("usuario")) 
+let nomeUsuario = `<h2 class="pixxelari">${usuario.nome}</h2>`
+
+if (usuario.avatar !== ""){
+    let avtHtml = `<img src="/src/img/icones/${usuario.avatar}.png" class="card-img-top" alt="...">`
+    document.getElementById("imagem-de-perfil").classList.add("d-none")
+    document.getElementById("imagem-avatar").innerHTML = avtHtml
+}
+
+
+document.getElementById("perfil-card-text").innerHTML = nomeUsuario
+
+URL = "http://localhost:3002/usuarios"; //Usuários cadastrados
+
+document.getElementById("salvar").addEventListener("click",(evt) => {
+    evt.preventDefault();
+    console.log(document.getElementById('nome').value,
+    document.getElementById('senha').value
+    )
+    if(document.getElementById("senha").value === document.getElementById("confirmar-senha").value){
+        fetch(`${URL}/${usuario.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: usuario.id,
+                nome: document.getElementById('nome').value,
+                senha: document.getElementById('senha').value
+            })
+        })
+        .then(res => res.json())
+        .then(resposta => {
+            window.localStorage.setItem("usuario", JSON.stringify({
+                id: usuario.id,
+                nome: document.getElementById('nome').value,
+                senha: document.getElementById('senha').value
+            }))
+        })
+        .then(() => location.reload())
+        .catch(error => console.error(error));
+    } else{
+        alert("Senhas não são idênticas, tente novamente.")
+    }
+})
+  
---
+++ src/js/troca-icone.js
@@ -0,0 +1,65 @@
+let usuario = JSON.parse(window.localStorage.getItem("usuario")) 
+
+URL = "http://localhost:3002/usuarios"; //Usuários cadastrados
+
+let avt = ""
+
+if(usuario.avatar !== ""){
+    let avtHtml = `<img
+    src="/src/img/icones/${usuario.avatar}.png"
+    class="card-img-top"
+    id="imagem-de-avatar"
+    alt="Avatar"
+    height="200"
+    />`
+    document.getElementById("imagem-de-perfil").classList.add("d-none")
+    document.getElementById("imagem-avatar").innerHTML = avtHtml
+}
+
+function trocarImagemAvatar(avatar) {
+    avt = avatar
+    let avatarHtml = `<img
+src="/src/img/icones/${avatar}.png"
+class="card-img-top"
+id="imagem-de-avatar"
+alt="Avatar"
+height="200"
+/>`
+if (document.getElementById("imagem-de-perfil") !== null){
+    document.getElementById("imagem-de-perfil").classList.add("d-none")
+    document.getElementById("imagem-avatar").innerHTML = avatarHtml
+} else {
+    document.getElementById("imagem-avatar").innerHTML = avatarHtml
+}
+
+}
+
+function salvar (){
+if (document.getElementById("imagem-de-perfil") === null){
+    fetch(`${URL}/${usuario.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: usuario.id,
+            nome: usuario.nome,
+            senha: usuario.senha,
+            avatar: avt
+        })
+    })
+    .then(res => res.json())
+    .then(resposta => {
+        window.localStorage.setItem("usuario", JSON.stringify({
+            id: usuario.id,
+            nome: usuario.nome,
+            senha: usuario.senha,
+            avatar: avt
+        }))
+        window.location.href = "http://127.0.0.1:5500/src/edicao-perfil.html";
+    })
+    .catch(error => console.error(error));
+} else {
+    alert ("Selecione um avatar antes de salvar")
+}
+}
