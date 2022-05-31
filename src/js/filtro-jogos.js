0f4e9a1e6e64e6363903df10197f5597ca0f31dd
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-05-30 21:37:14 -0300

    ajustes filtro js

--- src/js/filtro-jogos.js
+++ src/js/filtro-jogos.js
@@ -90,4 +90,81 @@ window.addEventListener("scroll", () => {
   }
 });
 
-showJogos();
+this.showJogos();
+
+function filtrarJogos() {
+    console.log("teste");
+
+    lista_jogos.innerHTML = '';
+
+  // input Nome do jogo
+  let nome = document.getElementById("nome_jogo");
+  // radio de Gêneros dos jogos
+  let genero = document.querySelector('input[name="radio-genero"]:checked').value;
+
+  // radio de Plataformas dos jogos
+  let plataforms = document.querySelector('input[name="radio-genero"]:checked').value;
+
+  limit = 100;
+  page = 1;
+
+  if(genero == null && plataforms == null){
+    URL = `http://localhost:3001/jogos?_limit=${limit}&_page=${page}`;
+  } else if(genero != null && plataforms == null) {
+    URL = `http://localhost:3001/jogos?_limit=${limit}&_page=${page}&genres=${genero}`;
+  } else if(genero == null && plataforms != null) {
+    URL = `http://localhost:3001/jogos?_limit=${limit}&_page=${page}&plataforms=${plataforms}`;
+  } else {
+    URL = `http://localhost:3001/jogos?_limit=${limit}&_page=${page}&genres=${genero}&plataforms=${plataforms}`;
+  }
+
+  fetch(URL)
+    .then((res) => res.json())
+    .then((jogos) => {
+        jogos.forEach((jogo) => {
+            let plataformas = "";
+        
+            if (jogo.plataforms != null || jogo.plataforms !== []) {
+              jogo.plataforms.forEach((plataforma) => {
+                if (plataforma === "Windows") plataformas = plataformas + icone_win;
+                if (plataforma === "MacBook") plataformas = plataformas + icone_mac;
+                if (plataforma === "Linux") plataformas = plataformas + icone_lin;
+              });
+            }
+        
+            const jogoEl = document.createElement("div");
+            jogoEl.classList.add("jogos");
+            jogoEl.classList.add("col-sm-12");
+            jogoEl.classList.add("col-md-12");
+            jogoEl.classList.add("col-lg-4");
+            jogoEl.classList.add("mb-3");
+            jogoEl.innerHTML = `
+        
+              <div class="card shadow-lg">
+                <h5 class="h6 card-title titulo-jogo-card">${jogo.title}</h5>
+                <img src="${jogo.img_sm}" class="card-img-top" alt="${jogo.title}">
+                <div class="card-body corpo-jogo-card">
+                  <div class="d-flex justify-content-between">
+                    <div>
+                      ${plataformas}
+                    </div>
+                    <div>
+                      <i class="fa-regular fa-heart texto-vermelho"></i>
+                      <a href="${jogo.steam_link}" target="_blank"><i class="fa-solid fa-paper-plane texto-azul"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <br>
+                <div class="d-flex justify-content-center pb-3">
+                  <a href="http://localhost:3001/jogos/${jogo.id}" class="btn btn-vermelho text-white link-jogo">Mais informações</a>
+                </div>
+              </div>
+                `;
+        
+            lista_jogos.appendChild(jogoEl);
+        })
+    })
+    .catch(err => console.error(err));
+}
+
+
